56f403d555776dea37a3904570de321643ac548f
Dwi Sasongko <dwiprasa@gmail.com>
2024-06-05 08:38:13 +0700

    add error form

--- js/script.js
+++ js/script.js
@@ -5,18 +5,10 @@ Jakarta, 02/06/2024
 */
 
 function replaceNama() {
-    let nama = prompt("Siapa nama Anda?","");
+    let nama = prompt("Masukkan nama Anda?","");
     document.getElementById("nama-header").innerHTML = nama;
 }
-
 document.getElementById("tombol").addEventListener("click", function() {replaceNama()});
-
-// function kirimForm() {
-//     const namanya = document.forms["myForm"]["fnama"].value;
-//     // document.getElementById("hasil-nama").innerHTML = namanya;
-//     console.log("namanya");
-//     return true;
-// }
 
 function kirimForm() {
     const tglSaiki = new Date();
@@ -25,12 +17,14 @@ function kirimForm() {
     const jkelnya = document.forms["myForm"]["fjkel"].value;
     const pesannya = document.forms["myForm"]["fpesan"].value;
     if(namanya == "" || tglnya == "" || jkelnya == "" || pesannya == ""){
+        document.getElementById("hasil-error").innerHTML = "<b>*Form tidak boleh kosong.</b>";
         document.getElementById("hasil-tglsaiki").innerHTML = "";
         document.getElementById("hasil-nama").innerHTML = "";
         document.getElementById("hasil-tgl").innerHTML = "";
         document.getElementById("hasil-jkel").innerHTML = "";
         document.getElementById("hasil-pesan").innerHTML = "";
     } else {
+        document.getElementById("hasil-error").innerHTML = "";
         document.getElementById("hasil-tglsaiki").innerHTML = "<b>Tanggal Sekarang : </b><br>" + tglSaiki;
         document.getElementById("hasil-nama").innerHTML = "<b>Nama : </b>" + namanya;
         document.getElementById("hasil-tgl").innerHTML = "<b>Tanggal Lahir : </b>" + tglnya;
@@ -38,5 +32,4 @@ function kirimForm() {
         document.getElementById("hasil-pesan").innerHTML = "<b>Pesan : </b>" + pesannya;    
     }    
 }
-
 document.getElementById("submit").addEventListener("click", function() {kirimForm()});
